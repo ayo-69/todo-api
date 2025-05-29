88ff1d22dd04474e3a2a530b677c85f1400d6d0e
Isaac Hayab <isaachayab0@gmail.com>
2025-05-29 12:56:06 +0100

    Added DELETE and UPDATE to /users

--- routes/users.js
+++ routes/users.js
@@ -33,4 +33,40 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// Updates /users/:id Updates user
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {name, email, password} = req.body;
+        const result = await pool.query("UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $3 RETURNING *", [name, email, password]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User not found"});
+        }
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server Error"})
+    }
+});
+
+// DELETE /users/:id Deletes user
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {name, email, password} = req.body;
+        const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User not found"});
+        }
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server Error"})
+    }
+});
+
 module.exports = router;
